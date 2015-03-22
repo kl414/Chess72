7030ac30723145e451aea21cf42563a313eb9300
kl414 <kevinlin414@gmail.com>
2015-03-22 14:38:36 -0400

    unfinished reading input

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -1,4 +1,9 @@
 package chess;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * 
  * @author Hongjie Lin
@@ -8,5 +13,37 @@ public class Chess {
 	public static void main(String[] args){
 		Board board = new Board();
 		board.display();
+		String input = wMove();
+		System.out.println();
+	}
+	
+	public static boolean isValid(String input){
+		
+	}
+	
+	public static String wMove(){
+		System.out.println();
+		System.out.print("White's move:");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			 return br.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static String bMove(){
+		System.out.println();
+		System.out.print("Black's move:");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			return br.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
